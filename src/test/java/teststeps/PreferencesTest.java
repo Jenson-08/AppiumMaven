44aa80ff2d9d0753a8a8506b39ed36cf1598506f
Jenson-08 <jensonpg08@gmail.com>
2024-06-06 14:06:47 -0600

    LongPressGesture test

--- src/test/java/teststeps/PreferencesTest.java
+++ src/test/java/teststeps/PreferencesTest.java
@@ -1,13 +1,19 @@
 package teststeps;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import setUp.BaseTest;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.rmi.Remote;
 
 public class PreferencesTest extends BaseTest {
 
@@ -24,7 +30,23 @@ public class PreferencesTest extends BaseTest {
         Assert.assertEquals(popUp, "WiFi settings");
         driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"android:id/edit\"]")).sendKeys("test1");
         driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
-        //set wifi name
+
 
     }
+
+    @Test
+    public void LongPressGesture(){
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Expandable Lists\"]")).click();
+        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+        WebElement ele = driver.findElement((By.xpath("//android.widget.TextView[@text=\"People Names\"]")));
+        ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture", ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),"duration",2000));
+        String menuText = driver.findElement(By.id("android:id/title")).getText();
+        Assert.assertEquals(menuText,"Sample Menu");
+        Assert.assertTrue(driver.findElement(By.id("android:id/title")).isDisplayed());
+    }
+
+
+
+
 }
